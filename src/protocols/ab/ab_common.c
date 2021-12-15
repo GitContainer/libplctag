2c61710deab80ce0d8708aace5db0f69f0906b59
Kyle Hayes <kyle.hayes@gmail.com>
2021-12-14 18:12:48 -0800

    Starting to revert changes.  Omron does not support tag listing in the same way.

--- src/protocols/ab/ab_common.c
+++ src/protocols/ab/ab_common.c
@@ -580,7 +580,7 @@ int get_tag_data_type(ab_tag_p tag, attr attribs)
                 /* check for special tags. */
                 if(str_cmp_i(tmp_tag_name, "@raw") == 0) {
                     special_tag_rc = setup_raw_tag(tag);
-                } else if(str_str_cmp_i(tmp_tag_name, "@tags")) {
+                } else if(tag->plc_type != AB_PLC_OMRON_NJNX && str_str_cmp_i(tmp_tag_name, "@tags")) {
                     special_tag_rc = setup_tag_listing_tag(tag, tmp_tag_name);
                 } else if(str_str_cmp_i(tmp_tag_name, "@udt/")) {
                     if(tag->plc_type != AB_PLC_OMRON_NJNX) {
